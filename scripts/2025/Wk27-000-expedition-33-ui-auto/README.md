fb5199bd8a19fe9478962f6eaef512306ef680f3
Lan <lanhikarixx@gmail.com>
2025-07-05 18:27:49 +0300

    golgra skip dialog loop is intermittently functional

---
+++ scripts/2025/Wk27-000-expedition-33-ui-auto/README.md
@@ -0,0 +1,6 @@
+# What is this?
+
+The main thing is `golgra-skip-dialog.py` which repeatedly takes screenshots and uses CV2 to look for
+specific visual cues. The key is to automate the dialog between losing against golgra to increase immersion.
+
+This uses [ydotool](<https://github.com/ReimuNotMoe/ydotool>).
--- scripts/2025/Wk27-000-expedition-33-ui-auto/golgra-skip-dialog.py
+++ scripts/2025/Wk27-000-expedition-33-ui-auto/golgra-skip-dialog.py
@@ -10,9 +10,14 @@ from pathlib import Path
 
 YTOOL = "/home/lan/src/cloned/gh/ReimuNotMoe/ydotool/build/ydotool"
 SOCKET = "/home/lan/.ydotool_socket"
-TEMPLATE = os.path.join(os.path.dirname(__file__), "golgra-as-expected-dialog.png")
 CONFIDENCE = 0.93
 POLL_INTERVAL = 0.25
+
+CUE_GOLGRA_AS_EXPECTED = os.path.join(
+    os.path.dirname(__file__), "golgra-as-expected-dialog.png"
+)
+CUE_WHERE_IS_ESQUIE = os.path.join(os.path.dirname(__file__), "where-is-esquie.png")
+CUE_WELL_WELL = os.path.join(os.path.dirname(__file__), "golgra-well-well.png")
 
 
 def ydotool(*args):
@@ -21,8 +26,9 @@ def ydotool(*args):
     )
 
 
-def beep():
-    subprocess.run(["aplay", "/usr/share/sounds/sound-icons/glass-water-1.wav"])
+def beep(which=0):
+    if which == 0:
+        subprocess.run(["aplay", "/usr/share/sounds/sound-icons/glass-water-1.wav"])
 
 
 def take_screenshot():
@@ -80,7 +86,9 @@ def take_screenshot():
         path = None
         for _ in range(20):
             time.sleep(0.1)
+
             new = {f.name for f in shots_dir.glob("Screenshot*.png")} - before
+
             if new:
                 path = shots_dir / max(new)
                 for _ in range(10):
@@ -89,11 +97,21 @@ def take_screenshot():
                     if s1 == path.stat().st_size and s1 > 0:
                         break
                 break
+
         if not path or not path.exists():
             raise RuntimeError("Portal never produced a screenshot")
 
-        time.sleep(2.0)
-        img = cv2.imread(str(path))
+        time.sleep(1.4)
+
+        for i in range(10):
+            img = cv2.imread(str(path))
+
+            if img is not None:
+                # if i != 0:
+                #     print(f"{i} OK!")
+                break
+
+            time.sleep(0.2)
 
         try:
             path.unlink()
@@ -119,7 +137,7 @@ def take_screenshot():
             )
 
 
-def on_visual_cue(x: int, y: int) -> None:
+def on_golgra_as_expected() -> None:
     print("VISUAL CUE DETECTED!")
 
     beep()
@@ -139,65 +157,114 @@ def on_visual_cue(x: int, y: int) -> None:
     ydotool("key", "17:1")
     time.sleep(1.3)
     ydotool("key", "17:0")
-    time.sleep(0.3)
+    time.sleep(0.5)
 
     # Talk to Golgra
 
     # E Key
     ydotool("key", "18:1")
     ydotool("key", "18:0")
-    time.sleep(0.3)
+    time.sleep(1.0)
 
     # Skip Hi
 
     # F Key
     ydotool("key", "33:1")
     ydotool("key", "33:0")
-    time.sleep(0.3)
+
+    # time.sleep(2.0)
+
+    # It seems intermittent how long it takes until we can move in dialog?
+    # It's also intermittent what two two S presses are going to do??
+    if not wait_for_visual_cue(CUE_WELL_WELL, lambda: (), timeout=5.0):
+        print("BOOO")
+        return
+
+    beep()
+
+    # time.sleep(1.5)
 
     # Choose final dialog option
 
-    # W Key
-    ydotool("key", "17:1")
-    ydotool("key", "17:0")
-    time.sleep(0.3)
+    # # W Key
+    # ydotool("key", "17:1")
+    # time.sleep(0.3)
+    # ydotool("key", "17:0")
+    # time.sleep(1.0)
+
+    # S Key
+    ydotool("key", "31:1")
+    time.sleep(0.1)
+    ydotool("key", "31:0")
+    time.sleep(0.5)
+
+    # S Key
+    ydotool("key", "31:1")
+    ydotool("key", "31:0")
+    time.sleep(0.5)
 
     # F Key
     ydotool("key", "33:1")
     ydotool("key", "33:0")
-    time.sleep(0.3)
+    time.sleep(1.0)
 
-    # Golgra: Really? A Duel?
+    # # Golgra: Really? A Duel?
+
+    time.sleep(1.0)
 
     # F Key
     ydotool("key", "33:1")
     ydotool("key", "33:0")
-    time.sleep(0.3)
+    time.sleep(0.5)
 
-    # Select Fighter
+    # # Select Fighter
 
     # A Key
     ydotool("key", "30:1")
     ydotool("key", "30:0")
-    time.sleep(0.3)
+    time.sleep(0.5)
 
     # Hold ENTER Key
     ydotool("key", "28:1")
-    time.sleep(1.0)
+    time.sleep(1.5)
     ydotool("key", "28:0")
+    time.sleep(1.0)
+
+    # F Key
+    ydotool("key", "33:1")
+    ydotool("key", "33:0")
+    time.sleep(0.5)
+
+    time.sleep(1.5)
+
+    # Final beep, back to combat!
+    beep()
 
 
+def wait_for_visual_cue(
+    visual_cue_path: str, on_cue: Callable[[], None], timeout: float = 0.0
+) -> bool:
+    tmpl = cv2.imread(visual_cue_path, cv2.IMREAD_GRAYSCALE)
 
-def event_loop_for_visual_cue(on_visual_cue_callback: Callable[[int, int], None]):
-    tmpl = cv2.imread(TEMPLATE, cv2.IMREAD_GRAYSCALE)
     if tmpl is None:
-        raise FileNotFoundError(f"Template image not found: {TEMPLATE}")
+        raise FileNotFoundError(f"Template image not found: {visual_cue_path}")
     tw, th = tmpl.shape[::-1]
 
-    print("Watching for Golgra Chat...")
+    start = time.time()
+
+    found = False
 
     while True:
-        img = take_screenshot()
+        if timeout != 0 and time.time() - start > timeout:
+            break
+
+        try:
+            # t = time.time()
+            img = take_screenshot()
+            # print(f"this takes {time.time() - t}")
+        except RuntimeError as e:
+            print(e)
+            continue
 
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         res = cv2.matchTemplate(gray, tmpl, cv2.TM_CCOEFF_NORMED)
@@ -210,16 +277,18 @@ def event_loop_for_visual_cue(on_visual_cue_callback: Callable[[int, int], None]
             y, x = y // 2, x // 2
             # print(f"→ Visual (Transformed) detected at ({x}, {y})")
 
-            on_visual_cue_callback(x, y)
+            on_cue()
 
-            time.sleep(0.5)  # debounce
+            # We are done, we executed the event
+            found = True
+            break
 
         time.sleep(POLL_INTERVAL)
 
-
-def main():
-    event_loop_for_visual_cue(on_visual_cue)
+    return found
 
 
 if __name__ == "__main__":
-    main()
+    while True:
+        print("Watching for Golgra Chat...")
+        wait_for_visual_cue(CUE_GOLGRA_AS_EXPECTED, on_golgra_as_expected)
